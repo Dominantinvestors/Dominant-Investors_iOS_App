2f35b95e65d9b73c52b0c5dda76180c089e480ae
dsay <dsay@ciklum.com>
2018-08-27 11:42:18 +0300

    Add requests

--- Dominant Investors/Repositories/AccountsDataProvider.swift
+++ Dominant Investors/Repositories/AccountsDataProvider.swift
@@ -30,23 +30,12 @@ struct AccountsDataProvider: Repository, Syncable {
                 confirm: String,
                 inviterID: String?,
                 completion: @escaping Completion) {
-        
+        send(request: DMUserProfileModel.login(email, password)).responseJSON { response in
+            if let json = response.result.value as? [String : AnyObject] {
+                let token = json["token"] as? String
+                UserDefaults.standard.set(token, forKey: ConstantsUserDefaults.accessToken)
+            }
+            completion(response.response?.statusCode ?? 500, response.error?.localizedDescription)
+        }
     }
-        
-//
-//    func settings(completion: @escaping (Response<Settings>) -> Void)  {
-//        let trace = Performance.startTrace(name: "GetSettings")
-//        send(request: Settings.settings()).responseObject { (response: DataResponse<Settings>) -> Void in
-//            trace?.stop()
-//            completion(self.handler.handle(response))
-//        }
-//    }
-//
-//    func reqisterNotificationToken(_ user: User, _ token: String, completion: @escaping Completion)  {
-//        let trace = Performance.startTrace(name: "ReqisterNotificationToken")
-//        send(request: user.reqisterNotificationToken(token)).response { respounce in
-//            trace?.stop()
-//            completion(respounce.response?.statusCode ?? 500, respounce.error?.localizedDescription)
-//        }
-//    }
 }
--- Dominant Investors/Repositories/Requests/User+Request.swift
+++ Dominant Investors/Repositories/Requests/User+Request.swift
@@ -3,48 +3,37 @@ import Alamofire
 fileprivate struct Key {
     static let email = "email"
     static let password = "password"
-//    static let clientSecret = "clientsecret"
-//    static let shopid = "shopid"
-//    static let notificationstoken = "notificationstoken"
+    
+    static let password1 = "password1"
+    static let password2 = "password2"
+    static let firstName = "first_name"
+    static let lastName = "last_name"
 }
-//
-//extension User {
-//
-//    static func authorise(_ email: String, _ password: String) -> RequestProvider {
-//        let parameters: [String: Any] = [Key.userName: email,
-//                                         Key.password: password,
-//                                         Key.clientSecret: Environment.clientSecret,
-//                                         Key.shopid: Environment.shopid]
-//
-//        return URLEncodingRequestBuilder(path: Environment.validateuser, method: .post, parameters: parameters)
-//    }
-//
-//    func welcomePageRequest(with path: String ) -> RequestProvider {
-//        let parameters: [String: Any] = [Key.userName: email,
-//                                         Key.password: password,
-//                                         Key.clientSecret: Environment.clientSecret,
-//                                         Key.shopid: Environment.shopid]
-//
-//        return URLEncodingRequestBuilder(path: path, method: .post, parameters: parameters)
-//    }
-//
-//    func reqisterNotificationToken(_ token: String) -> RequestProvider {
-//        let parameters: [String: Any] = [Key.userName: email,
-//                                         Key.password: password,
-//                                         Key.clientSecret: Environment.clientSecret,
-//                                         Key.shopid: Environment.shopid,
-//                                         Key.notificationstoken: token]
-//
-//        return URLEncodingRequestBuilder(path: Environment.registerNotification, method: .post, parameters: parameters)
-//    }
-//}
 
 extension DMUserProfileModel {
     
     static func login(_ email: String, _ password: String) -> RequestProvider {
-        
         let parameters: [String: Any] = [Key.email: email,
                                          Key.password: password]
         return URLEncodingRequestBuilder(path: "http://dominant-investors.geeks.land/api/v1/accounts/login/", method: .post, parameters: parameters)
+    }
+    
+    static func registration(login: String,
+                             email: String,
+                             password: String,
+                             confirm: String,
+                             firstName: String,
+                             lastName: String) -> RequestProvider
+    {
+        let parameters: [String: Any] = [Key.email: email,
+                                         Key.password1: password,
+                                         Key.password2: confirm,
+                                         Key.firstName: firstName,
+                                         Key.lastName: lastName]
+        return URLEncodingRequestBuilder(path: "http://dominant-investors.geeks.land/api/v1/accounts/registration/", method: .post, parameters: parameters)
+    }
+    
+    static func logout() -> RequestProvider {
+        return URLEncodingRequestBuilder(path: "http://dominant-investors.geeks.land/api/v1/accounts/logout/", method: .post)
     }
 }
